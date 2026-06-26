44dace3f2a0bdf50bbd1280b98c7f769701d8529
Sohum Bhardwaj <sohumb999@gmail.com>
2026-06-26 12:49:40 -0400

    made a tracker class

---
+++ tracker.py
@@ -0,0 +1,13 @@
+"""
+This class process individual frames in an image
+"""
+
+import cv2
+import numpy as np
+
+class DotTracker:
+    """This class contains methods to process individual frames from the spindle video"""
+
+    def __init__(self):
+        print("initialized")
+    
